8dd240a3500afbe336f3dd5df7b8034ce587e94f
Sukru Senli <sukru.senli@inteno.se>
2013-04-11 15:43:43 +0200

    add luci-dect package

---
+++ applications/luci-dect/luasrc/controller/user/dect.lua
@@ -0,0 +1,47 @@
+module("luci.controller.user.dect", package.seeall)
+
+function index()
+	if not nixio.fs.access("/usr/bin/dectmngr") then
+		return
+	end
+
+	local page
+
+	page = entry({"user", "services", "dect"}, template("dect_status"), _("DECT"))
+	page.dependent = true
+
+	page = entry({"user", "services", "reg_start"}, call("reg_start"))
+	page = entry({"user", "services", "reg_def"}, call("reg_def"))
+	page = entry({"user", "services", "dect_status"}, call("dect_status"))
+end
+
+function reg_start()
+	luci.sys.exec("/sbin/dectreg > /dev/null &")
+	luci.http.redirect(luci.dispatcher.build_url("user/services/dect"))
+	return
+end
+
+function reg_def()
+	luci.sys.exec("cp /etc/dect/nvs_default /etc/dect/nvs")
+	luci.http.redirect(luci.dispatcher.build_url("user/services/dect"))
+	return
+end
+
+function dect_status()
+	local sys  = require "luci.sys"
+	local uci  = require "luci.model.uci".cursor()
+	local run = 0
+
+	if nixio.fs.access("/var/dectisregistering") then
+		run = 1
+	elseif nixio.fs.access("/var/dectisregistered") then
+		run = 2
+	end
+
+	local status = {
+		running = run
+	}
+
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(status)
+end
