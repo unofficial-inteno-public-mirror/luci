38bdc41ab326ec44242407ee18bc2f0fb455f9e5
Sukru Senli <sukru.senli@inteno.se>
2013-04-12 13:58:03 +0200

    add igmp snooping under Status refs #2930

--- applications/luci-mcpd/luasrc/controller/admin/mcpd.lua
+++ applications/luci-mcpd/luasrc/controller/admin/mcpd.lua
@@ -1,10 +1,18 @@
 module("luci.controller.admin.mcpd", package.seeall)
 
 function index()
-	require("luci.i18n")
-	luci.i18n.loadc("mcpd")
+	local page 
 	
-	local page = entry({"admin", "network", "mcpd"}, cbi("admin_network/mcpd"), luci.i18n.translate("IGMP Proxy"))
-	page.i18n = "mcpd"
+	page = entry({"admin", "network", "mcpd"}, cbi("admin_network/mcpd"), luci.i18n.translate("IGMP Proxy"))
 	page.dependent = true
+
+	pacge = entry({"admin", "status", "igmp"}, call("action_igmp"), _("IGMP Snooping"))
+
+end
+
+function action_igmp()
+	local igmp_snooping
+	luci.util.exec("/sbin/hrigmpsnp /proc/net/igmp_snooping /var/igmp_snooping_hr")
+	igmp_snooping = luci.util.exec("cat /var/igmp_snooping_hr")
+	luci.template.render("admin_status/igmp_snooping", {igmp_snooping=igmp_snooping})
 end
