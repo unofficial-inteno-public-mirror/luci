6cb8f76279d60202a21ce691d4606129679aea79
Jonas Höglund <jonas.hoglund@embeddednation.com>
2013-05-20 13:54:30 +0200

    Dect handset status

--- applications/luci-dect/luasrc/controller/admin/dect.lua
+++ applications/luci-dect/luasrc/controller/admin/dect.lua
@@ -10,20 +10,30 @@ function index()
 	page = entry({"admin", "services", "dect"}, template("dect_status"), _("DECT"))
 	page.dependent = true
 
-	page = entry({"admin", "services", "reg_start"}, call("reg_start"))
-	page = entry({"admin", "services", "dect_status"}, call("dect_status"))
+	page = entry({"admin", "services", "dect", "reg_state"}, call("reg_state"))
+	page = entry({"admin", "services", "dect", "status"}, call("status"))
+	page = entry({"admin", "services", "dect", "reg_start"}, call("reg_start"))
 end
 
 function reg_start()
 	luci.sys.exec("/usr/bin/dect -r > /dev/null &")
+	dect_status()	
 end
 
 
-function dect_status()
+function status()
+
+	local rv = luci.sys.exec("/root/dect -j")
+
+	luci.http.prepare_content("application/json")
+	luci.http.write(rv)
+end
+
+
+function reg_state()
 
 	local rv = {
 		reg_state = luci.sys.exec("/usr/bin/dect -s | grep reg_state | awk '{ print $2 }'"),
-		hset1 = luci.sys.exec("/usr/bin/dect -s | grep 'hset: 1'"),
 	}
 
 	luci.http.prepare_content("application/json")
