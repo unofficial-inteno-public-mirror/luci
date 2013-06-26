417a3050d5551e505c4eb90a8644a1172b058095
Strhuan Blomquist <strhuan@inteno.se>
2013-06-26 13:55:49 +0200

    change model to handel non vdsl device refs#3228

--- modules/admin-full/luasrc/model/cbi/admin_network/layer2_interface.lua
+++ modules/admin-full/luasrc/model/cbi/admin_network/layer2_interface.lua
@@ -1,4 +1,6 @@
-
+local ubus = require "ubus".connect()
+local specs=ubus:call("router", "quest", { info = "specs" })
+ubus:close()
 m = Map("layer2_interface", translate("xDSL Settings"), translate("xDSL Line settings and profiles")) 
 
 s = m:section(NamedSection, "capabilities", translate("Capabilities")) -- Configure atm interface
@@ -29,28 +31,54 @@ e.disabled = ""
 e = s:taboption("modulation",Flag, "VDSL2", translate("VDSL2"))
 e.enabled = "Enabled"
 e.disabled = ""
+if not specs then
+        e = s:taboption("profile",Flag, "8a", translate("8a"))  
+        e.enabled = "Enabled"                                    
+        e.disabled = ""                                          
+        e = s:taboption("profile",Flag, "8b", translate("8b"))  
+        e.enabled = "Enabled"                                   
+        e.disabled = ""                                         
+        e = s:taboption("profile",Flag, "8c", translate("8c"))   
+        e.enabled = "Enabled"                                    
+        e.disabled = ""                                          
+        e = s:taboption("profile",Flag, "8d", translate("8d"))      
+        e.enabled = "Enabled"                                       
+        e.disabled = ""                                             
+        e = s:taboption("profile",Flag, "12a", translate("12a"))    
+        e.enabled = "Enabled"                                    
+        e.disabled = ""                                             
+        e = s:taboption("profile",Flag, "12b", translate("12b"))    
+        e.enabled = "Enabled"                                       
+        e.disabled = ""                                             
+        e = s:taboption("profile",Flag, "17a", translate("17a"))    
+        e.enabled = "Enabled"                                        
+        e.disabled = ""
 
-e = s:taboption("profile",Flag, "8a", translate("8a"))
-e.enabled = "Enabled"
-e.disabled = ""
-e = s:taboption("profile",Flag, "8b", translate("8b"))
-e.enabled = "Enabled"
-e.disabled = ""
-e = s:taboption("profile",Flag, "8c", translate("8c"))
-e.enabled = "Enabled"
-e.disabled = ""
-e = s:taboption("profile",Flag, "8d", translate("8d"))
-e.enabled = "Enabled"
-e.disabled = ""
-e = s:taboption("profile",Flag, "12a", translate("12a"))
-e.enabled = "Enabled"
-e.disabled = ""
-e = s:taboption("profile",Flag, "12b", translate("12b"))
-e.enabled = "Enabled"
-e.disabled = ""
-e = s:taboption("profile",Flag, "17a", translate("17a"))
-e.enabled = "Enabled"
-e.disabled = ""
+else
+        if specs.vdsl then
+        e = s:taboption("profile",Flag, "8a", translate("8a"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        e = s:taboption("profile",Flag, "8b", translate("8b"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        e = s:taboption("profile",Flag, "8c", translate("8c"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        e = s:taboption("profile",Flag, "8d", translate("8d"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        e = s:taboption("profile",Flag, "12a", translate("12a"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        e = s:taboption("profile",Flag, "12b", translate("12b"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        e = s:taboption("profile",Flag, "17a", translate("17a"))
+        e.enabled = "Enabled"
+        e.disabled = ""
+        end
+end
 e = s:taboption("capabilities",Flag, "US0", translate("US0"))
 e.enabled = "on"
 e.disabled = "off"
