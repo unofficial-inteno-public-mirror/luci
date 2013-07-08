284adf36961d8e822663f703ae171623762e84fc
Björn Ulfson <bu@leissner.se>
2013-07-08 15:39:34 +0200

    Updated SIP status page, refs #3268

--- applications/luci-voice-client/luasrc/model/cbi/voice_status.lua
+++ applications/luci-voice-client/luasrc/model/cbi/voice_status.lua
@@ -1,38 +1,72 @@
 --[[
-Read SIP account status from /var/state/
-and present registration status for the SIP accounts
+Read SIP account status from asterisk and present registration status for the SIP accounts
 ]]--
 
 u = luci.model.uci:cursor()
-us = luci.model.uci:cursor_state()
 
--- Get registration status
 function get_status()
 	local status = {}
-	u:foreach ("voice_client", "sip_service_provider", 
-		function(s1)
-			if s1.enabled == "1" then
-				local s1registered = us:get("asterisk", s1['.name'], "sip_registry_registered")
-				if not s1registered then
-					s1registered = "unknown"
-				end
+	sip_registry = luci.sys.exec("asterisk -rx 'sip show registry'")
+	lines = string.split(sip_registry, "\n")
+	for _, line in ipairs(lines) do
+		host, port = line:sub(1,40):match("^%s*(sip%d+):(%d+)%s*$")
+		if host then
+			dnsmgr = line:sub(41, 47):match("^%s*(.-)%s*$")
+			acc_user = line:sub(48, 62):match("^%s*(.-)%s*$")
+			acc_refresh = line:sub(63, 70):match("^%s*(.-)%s*$")
+			acc_state = line:sub(71, 91):match("^%s*(.-)%s*$")
+			acc_regtime = line:sub(92):match("^%s*(.-)%s*$")
 
-				local s1request_sent = us:get("asterisk", s1['.name'], "sip_registry_request_sent")
-				if not s1request_sent then
-					s1request_sent = "unknown"
-				end
+			acc_name = u:get("voice_client", host, "name")
+			acc_domain = u:get("voice_client", host, "domain")
 
-				row = {
-					title = s1.name,
-					user = s1.user,
-					domain = s1.domain,
-					registered = s1registered,
-					request_sent = s1request_sent
-				}
-				status[s1['.name']] = row
+			if acc_state == "Registered" then
+				acc_state = "Yes"
+			else
+				acc_state = string.format("No (%s)", acc_state)
 			end
+
+			row = {
+				title = acc_name,
+				user = acc_user,
+				domain = acc_domain,
+				state = acc_state,
+				refresh = acc_refresh,
+				regtime = acc_regtime
+			}
+			status[host] = row
 		end
-	)
+	end
+	return status
+end
+
+-- Read sip registration status from asterisk and uci
+function get_status2()
+	local status = {}
+	sip_registry = luci.sys.exec("asterisk -rx 'sip show registry'")
+	lines = string.split(sip_registry, "\n")
+	for _, line in ipairs(lines) do
+		host, port, dnsmgr, acc_user, acc_refresh, acc_state, acc_regtime = line:match("(sip%d+):(%d+)%s+(%w+)%s+(%w+)%s+(%d+)%s+(%w+)%s+(.*)$")
+		if not host then
+			host, port, dnsmgr, acc_user, acc_refresh, acc_state = line:match("(sip%d+):(%d+)%s+(%w+)%s+(%w+)%s+(%d+)%s+(%w.*)$")
+			acc_regtime = "-"
+		end
+
+		if host then
+			acc_name = u:get("voice_client", host, "name")
+			acc_domain = u:get("voice_client", host, "domain")
+
+			row = {
+				title = acc_name,
+				user = acc_user,
+				domain = acc_domain,
+				state = acc_state,
+				refresh = acc_refresh,
+				regtime = acc_regtime
+			}
+			status[host] = row
+		end
+	end
 	return status
 end
 
@@ -45,7 +79,8 @@ s = m:section(Table, get_status(), "")
 s:option(DummyValue, "title", "SIP Account")
 s:option(DummyValue, "user", "User")
 s:option(DummyValue, "domain", "Domain")
-s:option(DummyValue, "registered", "Registered")
-s:option(DummyValue, "request_sent", "Registration request sent")
+s:option(DummyValue, "state", "Registered")
+s:option(DummyValue, "refresh", "Registration interval (s)")
+s:option(DummyValue, "regtime", "Registration time")
 
 return m
