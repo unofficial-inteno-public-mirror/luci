36eda828a74f0961189ca9b46caf677262f35d06
Björn Ulfson <bu@leissner.se>
2013-07-22 17:07:38 +0200

    Updated voice status page to use ami-tool data from ubus (last regtime). Last registration time will now show last time a successful registration was made, even if not currently registered. refs #3268

--- applications/luci-voice-client/luasrc/model/cbi/voice_status.lua
+++ applications/luci-voice-client/luasrc/model/cbi/voice_status.lua
@@ -2,12 +2,16 @@
 Read SIP account status from asterisk and present registration status for the SIP accounts
 ]]--
 
+require("luci.json")
 u = luci.model.uci:cursor()
-us = luci.model.uci:cursor_state()
 
 function get_status()
 	local status = {}
+
 	sip_registry = luci.sys.exec("asterisk -rx 'sip show registry'")
+	ubus_data = luci.sys.exec("ubus call asterisk sip")
+	json_data = luci.json.decode(ubus_data)
+
 	lines = string.split(sip_registry, "\n")
 	for _, line in ipairs(lines) do
 		host, port = line:sub(1,40):match("^%s*(sip%d+):(%d+)%s*$")
@@ -20,43 +24,16 @@ function get_status()
 
 			acc_name = u:get("voice_client", host, "name")
 			acc_domain = u:get("voice_client", host, "domain")
+			
+			if json_data then
+				acc_regtime = json_data[host].last_registration
+			end
 
 			if acc_state == "Registered" then
 				acc_state = "Yes"
 			else
 				acc_state = string.format("No (%s)", acc_state)
-				acc_regtime = us:get("asterisk", host, "sip_registry_last_register")
 			end
-
-			row = {
-				title = acc_name,
-				user = acc_user,
-				domain = acc_domain,
-				state = acc_state,
-				refresh = acc_refresh,
-				regtime = acc_regtime
-			}
-			status[host] = row
-		end
-	end
-	return status
-end
-
--- Read sip registration status from asterisk and uci
-function get_status2()
-	local status = {}
-	sip_registry = luci.sys.exec("asterisk -rx 'sip show registry'")
-	lines = string.split(sip_registry, "\n")
-	for _, line in ipairs(lines) do
-		host, port, dnsmgr, acc_user, acc_refresh, acc_state, acc_regtime = line:match("(sip%d+):(%d+)%s+(%w+)%s+(%w+)%s+(%d+)%s+(%w+)%s+(.*)$")
-		if not host then
-			host, port, dnsmgr, acc_user, acc_refresh, acc_state = line:match("(sip%d+):(%d+)%s+(%w+)%s+(%w+)%s+(%d+)%s+(%w.*)$")
-			acc_regtime = "-"
-		end
-
-		if host then
-			acc_name = u:get("voice_client", host, "name")
-			acc_domain = u:get("voice_client", host, "domain")
 
 			row = {
 				title = acc_name,
