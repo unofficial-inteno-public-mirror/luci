d313719c5d598d11a61196f585473c5113c3fb2b
Simon Kers <simon@inteno.se>
2013-10-30 16:45:42 +0100

    generate logs for download refs #3306

--- modules/admin-full/luasrc/model/cbi/admin_system/system.lua
+++ modules/admin-full/luasrc/model/cbi/admin_system/system.lua
@@ -110,6 +110,14 @@ if guser ~= "user" then
 	o:value(8, translate("Normal"))
 	o:value(9, translate("Warning"))
 
+	o = s:taboption("logging", Button, "systemlog", translate("Download logs"), translate("Get all system logs"))
+	function o.write()
+		local reader = ltn12_popen("system_report 2>/dev/null")
+		luci.http.header('Content-Disposition', 'attachment; filename="system_report-%s-%s.log"' % { luci.sys.hostname(), os.date("%Y-%m-%d")})
+		luci.http.prepare_content("text/plain")
+		luci.ltn12.pump.all(reader, luci.http.write)
+	end
+
 	--
 	-- Connection Status
 	--
@@ -235,6 +243,35 @@ if has_ntpd then
 		-- retain server list even if disabled
 		function o.remove() end
 
+	end
+end
+
+function ltn12_popen(command)
+	local fdi, fdo = nixio.pipe()
+	local pid = nixio.fork()
+
+	if pid > 0 then
+		fdo:close()
+		local close
+		return function()
+			local buffer = fdi:read(2048)
+			local wpid, stat = nixio.waitpid(pid, "nohang")
+			if not close and wpid and stat == "exited" then
+				close = true
+			end
+
+			if buffer and #buffer > 0 then
+				return buffer
+			elseif close then
+				fdi:close()
+				return nil
+			end
+		end
+	elseif pid == 0 then
+		nixio.dup(fdo, nixio.stdout)
+		fdi:close()
+		fdo:close()
+		nixio.exec("/bin/sh", "-c", command)
 	end
 end
 
