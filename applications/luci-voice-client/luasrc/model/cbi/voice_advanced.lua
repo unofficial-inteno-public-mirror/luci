65dbfa7906df06241106b0b08cc68f682cd6f78e
Björn Ulfson <bu@leissner.se>
2013-11-05 11:23:18 +0100

    voice_client: Fixed a problem with echo cancellation setting

--- applications/luci-voice-client/luasrc/model/cbi/voice_advanced.lua
+++ applications/luci-voice-client/luasrc/model/cbi/voice_advanced.lua
@@ -339,8 +339,7 @@ ptime:value("40", "40")
 ptime.default = "20"
 
 ecan = line:option(Flag, "echo_cancel", "Echo cancellation")
-ecan.disabled = 0
-ecan.enabled = 1
+ecan.rmempty = false
 
 vad = line:option(ListValue, "vad", "Voice Activity Detection")
 vad:value(0, "Off")
