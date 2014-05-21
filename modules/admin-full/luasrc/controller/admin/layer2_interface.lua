3a2c2c33161a6a03efdf2fcd1b99e78ae3382889
Strhuan Blomquist <strhuan@inteno.se>
2014-05-21 16:05:54 +0200

    support for C300 Layer2 pages

--- modules/admin-full/luasrc/controller/admin/layer2_interface.lua
+++ modules/admin-full/luasrc/controller/admin/layer2_interface.lua
@@ -1,24 +1,32 @@
 module("luci.controller.admin.layer2_interface", package.seeall)
 
 function index()
-	local ubus = require "ubus".connect()
-	local users = { "admin" }
+        local ubus = require "ubus".connect()
+        local users = { "admin" }
         local specs
 
-	if ubus then 
-		specs = ubus:call("router", "quest", { info = "specs" })
-		ubus:close()
-	end
+        if ubus then 
+                specs = ubus:call("router", "quest", { info = "specs" })
+                ubus:close()
+        end
 
-	for k, user in pairs(users) do
-		entry({user, "network", "layer2_interface"}, cbi("admin_network/layer2_interface"), "Layer 2 Interfaces", 11)
-		entry({user, "network", "layer2_interface", "layer2_interface"}, cbi("admin_network/layer2_interface"), "xDSL Settings", 1)
-		entry({user, "network", "layer2_interface", "layer2_interface_vlan"}, cbi("admin_network/layer2_interface_vlan"), "VLAN", 2)
-		entry({user, "network", "layer2_interface", "layer2_interface_adsl"}, cbi("admin_network/layer2_interface_adsl"), "ADSL", 3)
-		if not specs or specs.vdsl then
-		        entry({user, "network", "layer2_interface", "layer2_interface_vdsl"}, cbi("admin_network/layer2_interface_vdsl"), "VDSL", 4)
-		end                                                                                                                                        
-		entry({user, "network", "layer2_interface", "layer2_interface_ethernet"}, cbi("admin_network/layer2_interface_ethernet"), "Ethernet", 5)
-	end
+        for k, user in pairs(users) do
+                if not specs or specs.adsl then
+                        entry({user, "network", "layer2_interface"}, cbi("admin_network/layer2_interface"), "xDSL Settings", 11)
+                else
+                        entry({user, "network", "layer2_interface"}, cbi("admin_network/layer2_interface_ethernet"), "Layer 2 Interfaces", 11)
+                end
+
+                if not specs or specs.adsl then
+                        entry({user, "network", "layer2_interface", "layer2_interface"}, cbi("admin_network/layer2_interface"), "xDSL Settings", 1)
+                end
+                	entry({user, "network", "layer2_interface", "layer2_interface_vlan"}, cbi("admin_network/layer2_interface_vlan"), "VLAN", 2)
+                if not specs or specs.adsl then
+                        entry({user, "network", "layer2_interface", "layer2_interface_adsl"}, cbi("admin_network/layer2_interface_adsl"), "ADSL", 3)
+                end
+                if not specs or specs.vdsl then
+                        entry({user, "network", "layer2_interface", "layer2_interface_vdsl"}, cbi("admin_network/layer2_interface_vdsl"), "VDSL", 4)
+                end                                                                                                                                        
+                entry({user, "network", "layer2_interface", "layer2_interface_ethernet"}, cbi("admin_network/layer2_interface_ethernet"), "Ethernet", 5)
+        end
 end
-
