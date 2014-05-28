c9d871b1080c10ce80c9ccfbd7e42d9d99e82942
Sukru Senli <sukru.senli@inteno.se>
2014-05-28 11:21:37 +0200

    port_management: dynamically populate ethernet ports

--- applications/luci-port-management/luasrc/model/cbi/port_management.lua
+++ applications/luci-port-management/luasrc/model/cbi/port_management.lua
@@ -1,58 +1,33 @@
 local sys = require "luci.sys"
 
+local PORTS = { "eth0", "eth1", "eth2", "eth3", "eth4" }
+
 m = Map("ports", translate("Port Management"))
 
 s = m:section(TypedSection, "ethports", "Ethernet Ports")
 s.anonymous = true
 
 function interfacename(port)
-	return sys.exec(". /lib/network/config.sh && interfacename %s" %port) or " "
+	local intfname = sys.exec(". /lib/network/config.sh && interfacename %s" %port)
+	if intfname:len() > 0 then
+		return intfname
+	end
+	return nil
 end
 
-eth0 = s:option(ListValue, "eth0", translate("eth0 (%s)" %interfacename("eth0")))
-eth0:value("auto", "Auto-negotiation")
---eth0:value("1000FD", "1000Mb, Full Duplex")
---eth0:value("1000HD", "1000Mb, Half Duplex")
-eth0:value("100FD", "100Mb, Full Duplex")
-eth0:value("100HD", "100Mb, Half Duplex")
-eth0:value("10FD" , "10Mb,  Full Duplex")
-eth0:value("10HD" , "10Mb,  Half Duplex")
-
-eth1 = s:option(ListValue, "eth1", translate("eth1 (%s)" %interfacename("eth1")))
-eth1:value("auto", "Auto-negotiation")
---eth1:value("1000FD", "1000Mb, Full Duplex")
---eth1:value("1000HD", "1000Mb, Half Duplex")
-eth1:value("100FD", "100Mb, Full Duplex")
-eth1:value("100HD", "100Mb, Half Duplex")
-eth1:value("10FD" , "10Mb,  Full Duplex")
-eth1:value("10HD" , "10Mb,  Half Duplex")
-
-eth2 = s:option(ListValue, "eth2", translate("eth2 (%s)" %interfacename("eth2")))
-eth2:value("auto", "Auto-negotiation")
---eth2:value("1000FD", "1000Mb, Full Duplex")
---eth2:value("1000HD", "1000Mb, Half Duplex")
-eth2:value("100FD", "100Mb, Full Duplex")
-eth2:value("100HD", "100Mb, Half Duplex")
-eth2:value("10FD" , "10Mb,  Full Duplex")
-eth2:value("10HD" , "10Mb,  Half Duplex")
-
-eth3 = s:option(ListValue, "eth3", translate("eth3 (%s)" %interfacename("eth3")))
-eth3:value("auto", "Auto-negotiation")
---eth3:value("1000FD", "1000Mb, Full Duplex")
---eth3:value("1000HD", "1000Mb, Half Duplex")
-eth3:value("100FD", "100Mb, Full Duplex")
-eth3:value("100HD", "100Mb, Half Duplex")
-eth3:value("10FD" , "10Mb,  Full Duplex")
-eth3:value("10HD" , "10Mb,  Half Duplex")
-
-eth4 = s:option(ListValue, "eth4", translate("eth4 (%s)" %interfacename("eth4")))
-eth4:value("auto", "Auto-negotiation")
---eth4:value("1000FD", "1000Mb, Full Duplex")
---eth4:value("1000HD", "1000Mb, Half Duplex")
-eth4:value("100FD", "100Mb, Full Duplex")
-eth4:value("100HD", "100Mb, Half Duplex")
-eth4:value("10FD" , "10Mb,  Full Duplex")
-eth4:value("10HD" , "10Mb,  Half Duplex")
+for _, eport in ipairs(PORTS) do
+	if interfacename(eport) then
+		eth = s:option(ListValue, eport, "%s (%s)" %{eport, interfacename(eport)})
+		eth.rmempty = true
+		eth:value("auto", "Auto-negotiation")
+		--eth:value("1000FD", "1000Mb, Full Duplex")
+		--eth:value("1000HD", "1000Mb, Half Duplex")
+		eth:value("100FD", "100Mb, Full Duplex")
+		eth:value("100HD", "100Mb, Half Duplex")
+		eth:value("10FD" , "10Mb,  Full Duplex")
+		eth:value("10HD" , "10Mb,  Half Duplex")
+	end
+end
 
 return m
 
