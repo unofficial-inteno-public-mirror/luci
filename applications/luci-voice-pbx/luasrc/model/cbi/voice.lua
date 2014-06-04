5752e266fdeaa8e1e4b9b5c5afbdad701fab8cfe
Kasper Ingelvide <ki@leissner.se>
2014-06-04 10:56:54 +0200

    voice-pbx: support for uploading melody for music on hold, refs #3810

--- applications/luci-voice-pbx/luasrc/model/cbi/voice.lua
+++ applications/luci-voice-pbx/luasrc/model/cbi/voice.lua
@@ -2,7 +2,9 @@ local dsp = require "luci.dispatcher"
 
 m = Map ("voice_pbx", translate("Voice Settings"))
 
-brcm = m:section(TypedSection, "brcm_advanced")
-brcm.anonymous = true
+s = m:section(TypedSection, "moh", "Music on hold", "Upload a sound file to be played when a call is put on hold or placed in a queue")
+s.anonymous = true
+s.addremove = false
+upload = s:option(FileUpload, 'sound_file', 'Sound', "The sound file must be in GSM format with a sample rate of 8 kHz. Use <tt>sox source.wav -r 8000 -c 1 music.gsm</tt> to convert.")
 
 return m
