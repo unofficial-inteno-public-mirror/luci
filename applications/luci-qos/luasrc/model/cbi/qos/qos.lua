d8147e7109cd7819361a456ca016b22db49159b8
Sukru Senli <sukru.senli@inteno.se>
2014-06-25 11:40:34 +0200

    qos: add type of service field

--- applications/luci-qos/luasrc/model/cbi/qos/qos.lua
+++ applications/luci-qos/luasrc/model/cbi/qos/qos.lua
@@ -17,29 +17,29 @@ local fs = require "nixio.fs"
 
 m = Map("qos", translate("Quality of Service"),
 	translate("With <abbr title=\"Quality of Service\">QoS</abbr> you " ..
-		"can prioritize network traffic selected by addresses, " ..
+		"can prioritize network traffic selected by type of service, addresses, " ..
 		"ports or services."))
 
-s = m:section(TypedSection, "interface", translate("Interfaces"))
-s.addremove = true
-s.anonymous = false
+--s = m:section(TypedSection, "interface", translate("Interfaces"))
+--s.addremove = true
+--s.anonymous = false
 
-e = s:option(Flag, "enabled", translate("Enable"))
-e.rmempty = false
+--e = s:option(Flag, "enabled", translate("Enable"))
+--e.rmempty = false
 
-c = s:option(ListValue, "classgroup", translate("Classification group"))
-c:value("Default", translate("default"))
-c.default = "Default"
+--c = s:option(ListValue, "classgroup", translate("Classification group"))
+--c:value("Default", translate("default"))
+--c.default = "Default"
 
-s:option(Flag, "overhead", translate("Calculate overhead"))
+--s:option(Flag, "overhead", translate("Calculate overhead"))
 
-s:option(Flag, "halfduplex", translate("Half-duplex"))
+--s:option(Flag, "halfduplex", translate("Half-duplex"))
 
-dl = s:option(Value, "download", translate("Download speed (kbit/s)"))
-dl.datatype = "and(uinteger,min(1))"
+--dl = s:option(Value, "download", translate("Download speed (kbit/s)"))
+--dl.datatype = "and(uinteger,min(1))"
 
-ul = s:option(Value, "upload", translate("Upload speed (kbit/s)"))
-ul.datatype = "and(uinteger,min(1))"
+--ul = s:option(Value, "upload", translate("Upload speed (kbit/s)"))
+--ul.datatype = "and(uinteger,min(1))"
 
 s = m:section(TypedSection, "classify", translate("Classification Rules"))
 s.template = "cbi/tblsection"
@@ -53,6 +53,18 @@ t:value("Express", translate("express"))
 t:value("Normal", translate("normal"))
 t:value("Bulk", translate("low"))
 t.default = "Normal"
+
+tos = s:option(ListValue, "tos", translate("ToS"))
+tos.rmempty = true
+tos:value("", translate("all"))
+tos:value("0")
+tos:value("1")
+tos:value("2")
+tos:value("3")
+tos:value("4")
+tos:value("5")
+tos:value("6")
+tos:value("7")
 
 srch = s:option(Value, "srchost", translate("Source host"))
 srch.rmempty = true
@@ -94,6 +106,6 @@ ports = s:option(Value, "ports", translate("Ports"))
 ports.rmempty = true
 ports:value("", translate("all"))
 
-bytes = s:option(Value, "connbytes", translate("Number of bytes"))
+--bytes = s:option(Value, "connbytes", translate("Number of bytes"))
 
 return m
