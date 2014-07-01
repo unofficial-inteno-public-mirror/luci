b7342c3c8d75f1cd082acbe47f3da17519c4de43
Kasper Ingelvide <ki@leissner.se>
2014-07-01 10:32:14 +0200

    WIP voice merge #4857

--- applications/luci-voice/root/usr/lib/asterisk/log_call.sh
+++ applications/luci-voice/root/usr/lib/asterisk/log_call.sh
@@ -7,10 +7,11 @@
 # at 100 rows. Some very basic concurrency is supported.
 #
 # Usage:
-# log_call.sh Direction Number
+# log_call.sh Direction Number [Note]
 #
-# Example:
+# Examples:
 # ./log_call Incoming 07012345678
+# ./log_call Outgoing 07876543210 Blocked
 #####################################################
 
 #Check that there is a direction specified
@@ -29,7 +30,11 @@ now=$(date)
 
 for i in 1 2 3 4 5 ; do
 	if mkdir /var/lock/log_call.lck ; then
-		echo "$now;$1;$2" >> $tempfile
+		if ! [ -z $3 ] ; then
+			echo "$now;$1;$2;$3" >> $tempfile
+		else
+			echo "$now;$1;$2" >> 	$tempfile
+		fi
 		head -n99 $logfile >> $tempfile
 		mv $tempfile $logfile
 		rmdir /var/lock/log_call.lck
