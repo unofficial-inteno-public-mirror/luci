0ab767285947902695be988330f460df3fbd690e
Kasper Ingelvide <ki@leissner.se>
2014-08-15 14:33:11 +0200

    voice: country selection sorted alphabetically, refs #5210

--- applications/luci-voice/luasrc/model/cbi/voice/country.lua
+++ applications/luci-voice/luasrc/model/cbi/voice/country.lua
@@ -9,36 +9,40 @@ end
 brcm = m:section(TypedSection, "brcm_advanced")
 brcm.anonymous = true
 
-countries = {	AUS = "AUSTRALIA",
-		BEL = "BELGIUM",
-		BRA = "BRAZIL",
-		CHL = "CHILE",
-		CHN = "CHINA",
-		CZE = "CZECH",
-		DNK = "DENMARK",
-		ETS = "ETSI",
-		FIN = "FINLAND",
-		FRA = "FRANCE",
-		DEU = "GERMANY",
-		HUN = "HUNGARY",
-		IND = "INDIA",
-		ITA = "ITALY",
-		JPN = "JAPAN",
-		NLD = "NETHERLANDS",
-		NZL = "NEW ZEALAND",
-		USA = "NORTH AMERICA",
-		ESP = "SPAIN",
-		SWE = "SWEDEN",
-		CHE = "SWITZERLAND",
-		NOR = "NORWAY",
-		TWN = "TAIWAN",
-		GBR = "UK",
-		ARE = "UNITED ARAB EMIRATES",
-		T57 = "CFG TR57" }
+countries = {
+	{ "AUS", "AUSTRALIA" },
+	{ "BEL", "BELGIUM" },
+	{ "BRA", "BRAZIL" },
+	{ "CHL", "CHILE" },
+	{ "CHN", "CHINA" },
+	{ "CZE", "CZECH" },
+	{ "DNK", "DENMARK" },
+	{ "ETS", "ETSI" },
+	{ "FIN", "FINLAND" },
+	{ "FRA", "FRANCE" },
+	{ "DEU", "GERMANY" },
+	{ "HUN", "HUNGARY" },
+	{ "IND", "INDIA" },
+	{ "ITA", "ITALY" },
+	{ "JPN", "JAPAN" },
+	{ "NLD", "NETHERLANDS" },
+	{ "NZL", "NEW ZEALAND" },
+	{ "USA", "NORTH AMERICA" },
+	{ "ESP", "SPAIN" },
+	{ "SWE", "SWEDEN" },
+	{ "CHE", "SWITZERLAND" },
+	{ "NOR", "NORWAY" },
+	{ "TWN", "TAIWAN" },
+	{ "GBR", "UK" },
+	{ "ARE", "UNITED ARAB EMIRATES" },
+	{ "T57", "CFG TR57" }
+}
+
+table.sort(countries, function(a, b) return a[2] < b[2] end)
 
 country = brcm:option(ListValue, 'country', 'Locale selection', 'A restart is required for country changes to take effect')
-for k,v in pairs(countries) do
-	country:value(k, v)
+for k,v in ipairs(countries) do
+	country:value(v[1], v[2])
 end
 country.default = "SWE"
 
