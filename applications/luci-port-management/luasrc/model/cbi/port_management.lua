567b22c79284affdc39258ffa358c1a10fa0a811
Sukru Senli <sukru.senli@inteno.se>
2015-01-08 14:32:34 +0100

    fix for non-working port management page refs #5881

--- applications/luci-port-management/luasrc/model/cbi/port_management.lua
+++ applications/luci-port-management/luasrc/model/cbi/port_management.lua
@@ -1,6 +1,7 @@
 local sys = require "luci.sys"
+local utl = require "luci.util"
 
-local PORTS = { "eth0", "eth1", "eth2", "eth3", "eth4" }
+local PORTS = utl.execi("db get hw.board.ethernetPortOrder | tr ' ' '\n'")
 
 m = Map("ports", translate("Port Management"))
 
@@ -19,10 +20,11 @@ function is_ethwan(port)
 	return (sys.exec("uci get layer2_interface_ethernet.Wan.baseifname"):match(port))
 end
 
-for _, eport in ipairs(PORTS) do
+for eport in PORTS do
 	if interfacename(eport) then
 		if ADMINST or not sys.exec("uci -q get ports.@ethports[0].%s" %eport):match("disabled") then
-			eth = s:option(ListValue, eport, "%s (%s)" %{eport, interfacename(eport)})
+			local detail = (TECUSER) and " (%s)" %eport or ""
+			eth = s:option(ListValue, eport, "%s%s" %{interfacename(eport), detail})
 			eth.rmempty = true
 			eth:value("auto", "Auto-negotiation")
 			--eth:value("1000FD", "1000Mb, Full Duplex")
