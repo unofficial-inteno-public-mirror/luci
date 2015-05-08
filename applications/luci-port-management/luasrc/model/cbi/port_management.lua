b7524a76198c738b24a52b395c57eaf2bf555474
Ronny Nilsson <ronny.nilsson@inteno.se>
2015-05-08 15:46:25 +0200

    Add support for issue refs #7176 where a maximum auto negotiated ethernet speed can be set.

--- applications/luci-port-management/luasrc/model/cbi/port_management.lua
+++ applications/luci-port-management/luasrc/model/cbi/port_management.lua
@@ -26,13 +26,19 @@ for eport in PORTS do
 			local detail = (TECUSER) and " (%s)" %eport or ""
 			eth = s:option(ListValue, eport, "%s%s" %{interfacename(eport), detail})
 			eth.rmempty = true
-			eth:value("auto", "Auto-negotiation")
-			--eth:value("1000FD", "1000Mb, Full Duplex")
-			--eth:value("1000HD", "1000Mb, Half Duplex")
-			eth:value("100FD", "100Mb, Full Duplex")
-			eth:value("100HD", "100Mb, Half Duplex")
-			eth:value("10FD" , "10Mb,  Full Duplex")
-			eth:value("10HD" , "10Mb,  Half Duplex")
+			eth:value("auto", "Full auto-negotiation")
+			--eth:value("1000FDAUTO", "Max 1000Mb auto-negotiation, full duplex")
+			--eth:value("1000HDAUTO", "Max 1000Mb auto-negotiation, half duplex")
+			eth:value("100FDAUTO", "Max 100Mb auto-negotiation, full duplex")
+			eth:value("100HDAUTO", "Max 100Mb auto-negotiation, half duplex")
+			eth:value("10FDAUTO", "Max 10Mb auto-negotiation, full duplex")
+			eth:value("10HDAUTO", "Max 10Mb auto-negotiation, half duplex")
+			--eth:value("1000FD", "Only 1000Mb, Full Duplex")
+			--eth:value("1000HD", "Only 1000Mb, Half Duplex")
+			eth:value("100FD", "Only 100Mb, Full Duplex")
+			eth:value("100HD", "Only 100Mb, Half Duplex")
+			eth:value("10FD" , "Only 10Mb,  Full Duplex")
+			eth:value("10HD" , "Only 10Mb,  Half Duplex")
 			if ADMINST then
 				eth:value("disabled" , "Disabled")
 			end
