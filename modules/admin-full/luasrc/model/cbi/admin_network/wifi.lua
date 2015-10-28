160dccc5a3143e73ba791ce28c7643abc169e28a
Sukru Senli <sukru.senli@inteno.se>
2015-10-28 11:09:01 +0100

    wifi beamforming default is 0

--- modules/admin-full/luasrc/model/cbi/admin_network/wifi.lua
+++ modules/admin-full/luasrc/model/cbi/admin_network/wifi.lua
@@ -253,7 +253,7 @@ if TECUSER then
 
 	if wdev:is_5g() then
 		bf = s:taboption("advanced", Flag, "beamforming", translate("Beamforming"))
-		bf.default = "1"
+		bf.default = "0"
 		bf.rmempty = true
 	end
 
