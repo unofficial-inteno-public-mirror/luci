b6a3e17fdc46efaa8f769ce70e9d529be6f38da4
Sukru Senli <sukru.senli@inteno.se>
2015-11-27 12:46:21 +0100

    add get rpc methods button to cwmp page

--- applications/luci-iup/luasrc/model/cbi/provisioning/cwmp.lua
+++ applications/luci-iup/luasrc/model/cbi/provisioning/cwmp.lua
@@ -1,14 +1,21 @@
 local net = require "luci.model.network".init()
-m = Map("cwmp",translate("TR-069"))
-
-
 local bit = require "bit"
 local uci = require("luci.model.uci").cursor()
+
+m = Map("cwmp",translate("TR-069"))
+
+function m.parse(map)
+	if m:formvalue("cbid.cwmp.acs.__rpc") then
+		luci.sys.exec("ubus call tr069 inform '{\"GetRPCMethods\": true}'")
+		luci.http.redirect(luci.dispatcher.build_url("admin/system/iup/cwmp"))
+		return
+	end
+	Map.parse(map)
+end
+
 s = m:section(NamedSection, "acs",  translate("Configure ACS specific settings"),translate("Configure ACS specific settings"))
 s.addremove = false
 s.anonymous = false
-
-
 
 n = s:option(Value, "userid", translate("ACS User Name"))
 n = s:option(Value, "passwd",translate("ACS Password") )
@@ -19,9 +26,16 @@ n.disabled = "false" and "0"
 n = s:option(Flag, "dhcp_discovery",translate("DHCP Discovery"), translate("Use option 43 to find URL of ACS"))
 n.enable="enable"
 n.disabled = "disable"
-n = s:option(Value, "url",translate("URL") )
+n = s:option(Value, "url", translate("URL"))
 
 n = s:option(Value, "periodic_inform_interval",translate("Periodic Inform Interval") )
+
+if m.uci:get("cwmp", "acs", "url") ~= nil then
+rpc = s:option(Button, "__rpc")
+rpc.title      = translate(" ")
+rpc.inputtitle = translate("Get RPC Methods")
+rpc.inputstyle = "apply"
+end
 
 s = m:section(NamedSection, "cpe",  translate("Configure CPE specific settings"),translate("Configure CPE specific settings"))
 s.addremove = false
@@ -39,8 +53,5 @@ uci:foreach("network", "interface",
 n = s:option(Value, "userid",translate("Connection Request User Name") )
 n = s:option(Value, "passwd",translate("Connection Request User Password") )  
 n = s:option(Value, "port",translate("Port") )
-
-
-
 
 return m
