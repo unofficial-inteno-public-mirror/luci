f77280120f24d2ab699b49e6c9705c844ac5bfbb
Sukru Senli <sukru.senli@inteno.se>
2016-02-26 11:50:17 +0100

    voice: populate ptimes from config instead of hardcoding

--- applications/luci-voice/luasrc/model/cbi/voice/sip_details.lua
+++ applications/luci-voice/luasrc/model/cbi/voice/sip_details.lua
@@ -213,11 +213,18 @@ s:option(Value, "displayname", "Display Name", "Display name used in Caller Id")
 
 -- Create and populate dropdowns with available codec choices                            
 codecs = {}
+ptimes = {}
 i = 0
 m.uci:foreach("voice_codecs", "supported_codec",
 	function(s1)
 		codecs[s1['.name']] = s1.name;
+		pmin = tonumber(s1.ptime_min) or 10
+		pmax = tonumber(s1.ptime_max) or 300
+		pdef = tonumber(s1.ptime_default) or 20
+		pinc = tonumber(s1.ptime_increment) or 10
+		ptimes[s1['.name']] = { min = pmin, max = pmax, default = pdef, increment = pinc }
 	end)
+
 for a, b in pairs(codecs) do
 	if i == 0 then
 		codec = s:option(ListValue, "codec"..i, "Preferred codecs")
@@ -236,17 +243,6 @@ for a, b in pairs(codecs) do
 
 	i = i + 1
 end
-
-ptimes = {
-	ulaw = {min = 10, max = 150, default = 20, increment = 10},
-	alaw = {min = 10, max = 150, default = 20, increment = 10},
-	g729 = {min = 10, max = 230, default = 20, increment = 10},
-	g723 = {min = 30, max = 300, default = 30, increment = 30},
-	g726 = {min = 10, max = 300, default = 20, increment = 10},
-	g722 = {min = 10, max = 300, default = 20, increment = 10},
-	gsm  = {min = 10, max = 300, default = 20, increment = 10},
-	ilbc = {min = 10, max = 300, default = 20, increment = 10}
-}
 
 for a, b in pairs(codecs) do
 	ptime = s:option(ListValue, "ptime_"..a, b.." packetization")
