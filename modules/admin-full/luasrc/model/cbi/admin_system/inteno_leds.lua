2734028ff7857fdc7a151d90e8c7669118d87fcb
Sukru Senli <sukru.senli@inteno.se>
2016-05-06 15:40:23 +0200

    use leds configuration for controlling LEDs

--- modules/admin-full/luasrc/model/cbi/admin_system/inteno_leds.lua
+++ modules/admin-full/luasrc/model/cbi/admin_system/inteno_leds.lua
@@ -1,9 +1,9 @@
-m = Map("boardpanel", "LEDs")
-s = m:section(NamedSection, "settings")
+m = Map("leds", "LEDs")
+s = m:section(NamedSection, "leds")
 s.title = "Configure LEDs"
 s.anonymous = true
 
-sl = s:option(Flag, "status_led", "Status LEDs")
+sl = s:option(Flag, "enable", "Status LEDs")
 sl.rmempty = false
 
 return m
